9c02fe8ceb887cf753c71a637ceed45a7dc955a7
barni246 <barnabas.gonda@googlemail.com>
2025-03-23 18:55:15 +0100

    api-auth

--- market_hub/urls.py
+++ market_hub/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/market/', include('market_app.api.urls')),
     # path('api/auth/', include('user_auth_app.api.urls')),
+    path('api-auth', include('rest_framework.urls')),
 ]
