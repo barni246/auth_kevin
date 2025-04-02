622c7b0cccdbd803904982f20e43e786b7e285fe
barni246 <barnabas.gonda@googlemail.com>
2025-04-02 21:19:34 +0200

    login, obtain_auth_token

--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -1,9 +1,11 @@
 #api/urls.py
 from django.urls import path
 from .views import UserProfileList, UserProfileDetail,RegistrationView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('profiles/', UserProfileList.as_view(), name='userprofile-list'),
     path('profiles/<int:pk>/', UserProfileDetail.as_view(), name='userprofile-detail'),
-    path('registration/', RegistrationView.as_view(),name='registration-view')
+    path('registration/', RegistrationView.as_view(),name='registration'),
+    path('login/', obtain_auth_token, name='login')
 ]
