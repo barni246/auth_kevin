896fc9b16823879ee80d895e484eee780e343793
barni246 <barnabas.gonda@googlemail.com>
2025-04-02 21:38:00 +0200

    CustomLoginView(ObtainAuthToken)

--- user_auth_app/api/views.py
+++ user_auth_app/api/views.py
@@ -7,6 +7,8 @@ from rest_framework.permissions import AllowAny
 from .serializers import RegistrationSerializer
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
+
 
 class UserProfileList(generics.ListCreateAPIView):
     queryset = UserProfile.objects.all()
@@ -15,6 +17,27 @@ class UserProfileList(generics.ListCreateAPIView):
 class UserProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+
+class CustomLoginView(ObtainAuthToken):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        data = {}
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, created= Token.objects.get_or_create(user=user)
+            data = {
+                'token':token.key,
+                'username':user.username,
+                'email':user.email,
+            }
+        else:
+            data=serializer.errors
+
+        return Response(data)
+    
 
 class RegistrationView(APIView):
     permission_classes = [AllowAny]
@@ -35,3 +58,4 @@ class RegistrationView(APIView):
             data=serializer.errors
 
         return Response(data)
+
